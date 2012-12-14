678b597f18ff0b2326b05597590c6e7b2d1ae413
birkin <birkin.diana@gmail.com>
2012-12-14 15:48:35 -0500

    added initial hardcoded template

--- views.py
+++ views.py
@@ -3,7 +3,7 @@
 import json, logging, pprint
 # import requests
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect  # , HttpResponseNotFound, 
-# from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response
 # from django.views.decorators.cache import cache_page
 # from usep_app import settings_app, models, utility_code
 from software_app import models, settings_app
@@ -20,12 +20,12 @@ assert sorted(dir(settings_app)[0:-5]) == [
 
 # @cache_page( 60 * 5 )  # 5 minutes
 def apps( request ):
-  # page_dict = { 
-  #   u'a': 1,
-  #   u'b': 2 
-  #   }
-  return HttpResponse( u'test', content_type=u'text/javascript; charset=utf8' )
-  # return render_to_response( u'usep_templates/collectionS.html', page_dict )
+  page_dict = { 
+    u'a': 1,
+    u'b': 2 
+    }
+  # return HttpResponse( u'test', content_type=u'text/javascript; charset=utf8' )
+  return render_to_response( u'software_app_templates/base.html', page_dict )
 
 
 def login( request ):
