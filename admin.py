34104e601410bb5001973b5b29be2e6960fe569e
birkin <birkin.diana@gmail.com>
2012-12-21 08:15:05 -0500

    autopopulated slug field

--- admin.py
+++ admin.py
@@ -9,6 +9,7 @@ class SoftwareAdmin(admin.ModelAdmin):
   list_display = [ 'name', 'audience', 'activity', 'contact_technical_email' ]
   list_filter = [ 'activity', 'api', 'audience', 'license_name' ]
   search_fields = [ 'name' ]
+  prepopulated_fields = { u'slug': (u'name',) }
   # readonly_fields = [ 'code', 'settlement_code', 'region_code' ]
   
 
